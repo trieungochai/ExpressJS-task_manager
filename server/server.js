82bcfc4e38268f0a7d0ac285a9602a1eb84c113d
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-21 21:03:28 +0700

    Connecting to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./database/connect");
+require("dotenv").config();
 
 const taskRouter = require("./routes/tasks.router");
 
@@ -10,6 +12,15 @@ app.use(express.json());
 // routes
 app.use("/api/v1/tasks", taskRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
