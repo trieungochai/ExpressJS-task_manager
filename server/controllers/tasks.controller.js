b3d72fb9e59289d749a80307b8e7df8ef122c326
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-21 21:59:12 +0700

    API create single post

--- server/controllers/tasks.controller.js
+++ server/controllers/tasks.controller.js
@@ -1,9 +1,29 @@
+const Task = require("../models/Task.model");
+
 const getAllTasks = (req, res) => {
   return res.send("Get all tasks");
 };
 
-const createTask = (req, res) => {
-  return res.send("Create task");
+const createTask = async (req, res) => {
+  const { name, completed } = req.body;
+  if (!name)
+    return res.status(400).json({
+      success: false,
+      message: "Task name is required",
+    });
+
+  try {
+    const newTask = new Task({ name, completed: completed || false });
+    await newTask.save();
+    return res
+      .status(201)
+      .json({ success: true, message: "Successfully created", task: newTask });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
 };
 
 const getSingleTask = (req, res) => {
--- server/models/Task.model.js
+++ server/models/Task.model.js
@@ -4,10 +4,13 @@ const Schema = mongoose.Schema;
 const TaskSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Task name is required"],
+    trim: true,
+    maxLength: [20, "Name can not be more than 20 characters"],
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
